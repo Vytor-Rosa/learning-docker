d04df5f58cc0fc9f0229a0fa8fa59d9a6c230bde
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-21 13:21:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.et("/", (req, res) => {res.send("hello world");});
-
+app.get("/", (req, res) => {res.send("hello world");});
+ 
 app.listen(port, () => {console.log("rodando na porta ", port)});
 
